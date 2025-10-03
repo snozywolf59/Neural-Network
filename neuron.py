37c2aa257606354dcd9b9c0cbeee433639d1ff7c
Bang <thbang727@gmail.com>
2025-10-03 10:59:55 +0700

    fix-neuron-bug-and-test-backprop-and-test-training

--- neuron.py
+++ neuron.py
@@ -1,29 +1,47 @@
+from abc import ABC, abstractmethod
+
 from value import Value
 from math import tanh
 
-class Neuron:
+class ParamModule(ABC):
+    @abstractmethod
+    def parameters(self) -> list:
+        pass
+    
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0.0
+    
+class Neuron(ParamModule):
     def __init__(self, dimension):
         import random
         self.w = [Value(random.uniform(-1, 1)) for _ in range (dimension)]
         self.b = Value(random.uniform(-1, 1))
-        self.f = lambda x: tanh(x)
+        self.f = lambda x: x.tanh()
     
     def activation(self, f):
         self.f = f
     
     def __call__(self, x):
         act = sum(((wi * xi) for wi, xi in zip(self.w, x)), self.b)
-        return self.f(act.val)
+        return self.f(act)
+    
+    def parameters(self):
+        return self.w + [self.b]
+    
         
-class Layer:
+class Layer(ParamModule):
     def __init__(self, d_in, d_out):
         self.neurons = [Neuron(dimension=d_in) for _ in range(d_out)]
     
     def __call__(self, x):
-        outs = [n(x) for n in self.neurons]
+        outs = [n(x) for n in self.neurons] if len(self.neurons) > 1 else self.neurons[0](x)
         return outs
+    
+    def parameters(self):
+        return [param for neuron in self.neurons for param in neuron.parameters()]
 
-class MLP:
+class MLP(ParamModule):
     def __init__(self, d_in, d_outs):
         sz = [d_in] + d_outs
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz) - 1)]
@@ -33,6 +51,31 @@ class MLP:
             x = layer(x)
         return x
     
+    def parameters(self):
+        return [param for layer in self.layers for param in layer.parameters()]
+    
 
-m = MLP(3, [6, 6, 3])
-print(m([1.0, 2.0, 3.0]))
+x = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0]]
+y = [1.0, -1.0, -1.0]
+mlp = MLP(3, [4, 8, 4, 1])
+
+
+y_pred = [mlp(xi) for xi in x]
+
+
+
+for epoch in range(100):
+    mlp.zero_grad()
+    y_pred = [mlp(xi) for xi in x]
+    loss = sum(( (yi_p - yi)**2 for yi, yi_p in zip(y, y_pred) ))
+    loss.grad = 1.0
+    loss.backward()
+    
+    learning_rate = 0.01 * (0.97 ** epoch) if epoch < 10 else 0.005 * (0.97 ** (epoch - 10))
+    
+    for p in mlp.parameters():
+        p.val += - learning_rate * p.grad
+    
+    print(f"Epoch {epoch}: loss {loss.val}")
+
+
--- value.py
+++ value.py
@@ -1,3 +1,5 @@
+import math
+
 class Value:
     def __init__(self, val, label='unknown', _children=(), _op=None):
         self.val = val
@@ -13,26 +15,42 @@ class Value:
     def __add__(self, other):
         assert isinstance(other, (int, float, Value)), "Only supports int/float/Value addition."
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.val + other.val, (self, other), '+')
+        out = Value(self.val + other.val, _children=(self, other), _op='+')
 
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
             
-            self._backward() if self._backward else None
-            other._backward() if other._backward else None
-            
         out._backward = _backward
 
         return out
     
+    def __radd__(self, other):        
+        return self + other
+    
+    def __sub__(self, other):
+        other  = other if isinstance(other, Value) else Value(other)
+        out = Value(self.val - other.val, _children=(self, other), _op='-')
+        def _backward():
+            self.grad += out.grad
+            other.grad +=  out.grad * -1
+            
+        out._backward = _backward
+        return out
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __rsub__(self, other):
+        return -(self - other)
+    
     def __pow__(self, other):
         assert isinstance(other, (int, float, Value)), "Only supports int/float/Value powers."
-        other = other.val if isinstance(other, Value) else other
-        out = Value(self.val ** other, (self,), f'**{other}')
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.val ** other.val, _children=(self,), _op=f'**{other}')
 
         def _backward():
-            self.grad += other * (self.val ** (other - 1)) * out.grad
+            self.grad += other.val * (self.val ** (other.val - 1)) * out.grad
         out._backward = _backward
 
         return out
@@ -40,12 +58,49 @@ class Value:
     def __mul__(self, other):
         assert isinstance(other, (int, float, Value)), "Only supports int/float/Value multiplication."
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.val * other.val, (self, other), '*')
+        out = Value(self.val * other.val,_children= (self, other),_op= '*')
 
         def _backward():
             self.grad += other.val * out.grad
             other.grad += self.val * out.grad
+            
         out._backward = _backward
 
         return out
     
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        return other * (self ** -1)
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
+    
+    def tanh(self):
+        x = self.val
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, _children=(self,), _op='tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+
+        return out
